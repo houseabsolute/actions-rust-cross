ab3f022734e560bf3e3bd422b34f2e08c53db1ca
Dave Rolsky <autarch@urth.org>
2023-07-22 09:43:11 -0500

    Remove anything related to installing cross on Windows
    There are no targets on Windows that require cross so this is never getting
    invoked.

--- install-cross-windows.ps1
+++
@@ -1,4 +0,0 @@
-cd $Env:RUNNER_TEMP
-Invoke-WebRequest -URI 'https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.ps1' -UseBasicParsing | Invoke-Expression
-.\ubi --project cross-rs/cross --in .
-
