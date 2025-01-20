f3ca96f7309c1f4eb5e38c57e681a440a61645b2
Dave Rolsky <autarch@urth.org>
2025-01-20 12:13:28 -0600

    Add `anyhow` dep to test project so we can see more action in the cache

--- test-project/src/bin1.rs
+++ test-project/src/bin1.rs
@@ -1,5 +1,15 @@
+use anyhow::Result;
+
 fn main() {
     println!("Hello, world!");
+    if let Err(e) = run_something() {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run_something() -> Result<()> {
+    Ok(())
 }
 
 #[cfg(test)]
